79556d24b785eb0ade6dfd0cd59248a669a0f2df
Trevor <trevorguidry6@gmail.com>
2020-02-19 20:35:05 -0500

    remove stuff

--- src/main/java/com/trevor/greenscreen/Triangle.java
+++ src/main/java/com/trevor/greenscreen/Triangle.java
@@ -35,20 +35,4 @@ public class Triangle
 	private final Vertex a;
 	private final Vertex b;
 	private final Vertex c;
-
-	/**
-	 * Rotates the triangle by the given orientation.
-	 *
-	 * @param orientation passed orientation
-	 * @return new instance
-	 */
-	public Triangle rotate(int orientation)
-	{
-		return new Triangle(
-			a.rotate(orientation),
-			b.rotate(orientation),
-			c.rotate(orientation)
-		);
-	}
-
 }
