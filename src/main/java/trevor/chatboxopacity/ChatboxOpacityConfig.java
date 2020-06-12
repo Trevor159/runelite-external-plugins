dbee9ba4e138cae2467354fc169d9c34dcac3162
Trevor <trevorguidry6@gmail.com>
2020-06-11 21:18:44 -0400

    add button background opacity config option

--- src/main/java/trevor/chatboxopacity/ChatboxOpacityConfig.java
+++ src/main/java/trevor/chatboxopacity/ChatboxOpacityConfig.java
@@ -10,14 +10,31 @@ public interface ChatboxOpacityConfig extends Config
 {
 	@ConfigItem(
 		keyName = "opacity",
-		name = "Opacity",
-		description = "The opacity of your chatbox from 0 to 255 (0 being black and 255 being transparent)"
+		name = "Chatbox Opacity",
+		description = "The opacity of your chatbox from 0 to 255 (0 being black and 255 being transparent)",
+		position = 1
 	)
 	@Range(
+		min = -1,
 		max = 255
 	)
-	default int opacity()
+	default int chatboxOpacity()
 	{
 		return 150;
+	}
+
+	@ConfigItem(
+		keyName = "buttonOpacity",
+		name = "Button Opacity",
+		description = "The opacity of your chatbox buttons from 0 to 255 (0 being black and 255 being transparent)",
+		position = 2
+	)
+	@Range(
+		min = -1,
+		max = 255
+	)
+	default int buttonOpacity()
+	{
+		return -1;
 	}
 }
--- src/main/java/trevor/chatboxopacity/ChatboxOpacityPlugin.java
+++ src/main/java/trevor/chatboxopacity/ChatboxOpacityPlugin.java
@@ -7,6 +7,7 @@ import net.runelite.api.GameState;
 import net.runelite.api.Varbits;
 import net.runelite.api.events.ScriptPostFired;
 import net.runelite.api.widgets.Widget;
+import static net.runelite.api.widgets.WidgetID.CHATBOX_GROUP_ID;
 import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
@@ -22,6 +23,10 @@ import net.runelite.client.plugins.PluginDescriptor;
 public class ChatboxOpacityPlugin extends Plugin
 {
 	private static final int BUILD_CHATBOX_SCRIPT = 923;
+	private static final int CHATBOX_GROUP = CHATBOX_GROUP_ID;
+	private static final int CHATBOX_BUTTON_BACKGROUND = 3;
+	private static final int ORIGINAL_BUTTON_BACKGROUND_TYPE = 5;
+	private static final int NEW_BUTTON_BACKGROUND_TYPE = 3;
 
 	@Inject
 	private Client client;
@@ -32,6 +37,9 @@ public class ChatboxOpacityPlugin extends Plugin
 	@Inject
 	private ClientThread clientThread;
 
+	private int chatboxButtonBackgroundSprite = -1;
+	private int previousChatboxOpacity = -1;
+
 	@Provides
 	ChatboxOpacityConfig provideConfig(ConfigManager configManager)
 	{
@@ -41,13 +49,28 @@ public class ChatboxOpacityPlugin extends Plugin
 	@Override
 	protected void startUp() throws Exception
 	{
-		clientThread.invoke(() -> writeChatboxOpacity(config.opacity()));
+		clientThread.invoke(() -> writeChatboxOpacity());
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		clientThread.invoke(() -> client.runScript(BUILD_CHATBOX_SCRIPT));
+		clientThread.invoke(() ->
+		{
+			if (chatboxButtonBackgroundSprite != -1)
+			{
+				Widget widget = client.getWidget(CHATBOX_GROUP, CHATBOX_BUTTON_BACKGROUND);
+				if (widget != null)
+				{
+					widget.setSpriteId(chatboxButtonBackgroundSprite);
+					widget.setFilled(false);
+					widget.setType(ORIGINAL_BUTTON_BACKGROUND_TYPE);
+					chatboxButtonBackgroundSprite = -1;
+				}
+			}
+			client.runScript(BUILD_CHATBOX_SCRIPT);
+
+		});
 	}
 
 	@Subscribe
@@ -58,7 +81,7 @@ public class ChatboxOpacityPlugin extends Plugin
 			return;
 		}
 
-		writeChatboxOpacity(config.opacity());
+		writeChatboxOpacity();
 	}
 
 	@Subscribe
@@ -66,11 +89,11 @@ public class ChatboxOpacityPlugin extends Plugin
 	{
 		if (event.getGroup().equals("chatboxopacity"))
 		{
-			clientThread.invoke(() -> writeChatboxOpacity(config.opacity()));
+			clientThread.invokeLater(() -> writeChatboxOpacity());
 		}
 	}
 
-	private void writeChatboxOpacity(int opacity)
+	private void writeChatboxOpacity()
 	{
 		if (client.getGameState() != GameState.LOGGED_IN
 			|| !client.isResized()
@@ -90,14 +113,49 @@ public class ChatboxOpacityPlugin extends Plugin
 
 		Widget[] children = widget.getChildren();
 
-		if (children.length != 20)
+		if (children.length == 20)
 		{
-			return;
+			if (config.chatboxOpacity() != -1)
+			{
+				for (Widget child : children)
+				{
+					child.setOpacity(config.chatboxOpacity());
+					previousChatboxOpacity = config.chatboxOpacity();
+				}
+			}
+			else if (previousChatboxOpacity != -1)
+			{
+				previousChatboxOpacity = -1;
+				client.runScript(BUILD_CHATBOX_SCRIPT);
+			}
 		}
 
-		for (Widget child : children)
+		if (config.buttonOpacity() != -1)
 		{
-			child.setOpacity(opacity);
+			widget = client.getWidget(CHATBOX_GROUP, CHATBOX_BUTTON_BACKGROUND);
+			if (widget != null)
+			{
+				if (chatboxButtonBackgroundSprite == -1)
+				{
+					chatboxButtonBackgroundSprite = widget.getSpriteId();
+					widget.setSpriteId(-1);
+					widget.setType(NEW_BUTTON_BACKGROUND_TYPE);
+					widget.setFilled(true);
+				}
+				widget.setOpacity(config.buttonOpacity());
+			}
+		}
+		else if (chatboxButtonBackgroundSprite != -1)
+		{
+			widget = client.getWidget(CHATBOX_GROUP, CHATBOX_BUTTON_BACKGROUND);
+			if (widget != null)
+			{
+				widget.setSpriteId(chatboxButtonBackgroundSprite);
+				widget.setFilled(false);
+				widget.setType(ORIGINAL_BUTTON_BACKGROUND_TYPE);
+				chatboxButtonBackgroundSprite = -1;
+				client.runScript(BUILD_CHATBOX_SCRIPT);
+			}
 		}
 	}
 }
